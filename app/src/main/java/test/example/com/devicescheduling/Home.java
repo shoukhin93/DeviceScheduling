604b24bbad7242a01491842478120751831d87da
shoukhin93 <shoukhin1993@gmail.com>
2018-07-09 19:06:59 +0600

    links connected with home page

--- app/src/main/java/test/example/com/devicescheduling/Home.java
+++ app/src/main/java/test/example/com/devicescheduling/Home.java
@@ -1,8 +1,7 @@
 package test.example.com.devicescheduling;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    Button setScheduleButton;
+    Button allowedNumbersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,23 @@ public class Home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setScheduleButton = findViewById(R.id.set_schedule_button);
+        allowedNumbersButton = findViewById(R.id.allowed_numbers_button);
+
+        setScheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, AddSchedule.class));
+            }
+        });
+
+        allowedNumbersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, AllowedNumbers.class));
+            }
+        });
     }
 
     @Override
@@ -43,42 +62,20 @@ public class Home extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_history) {
+            startActivity(new Intent(this, AlarmHistory.class));
+        } else if (id == R.id.nav_my_schedule) {
+            startActivity(new Intent(this, MyAlarms.class));
+        } else if (id == R.id.nav_add_schedule) {
+            startActivity(new Intent(this, AddSchedule.class));
+        } else if (id == R.id.nav_allowed_numbers) {
+            startActivity(new Intent(this, AllowedNumbers.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
