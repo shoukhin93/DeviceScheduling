27dfc8858c2a43d5681143ed4b1262a50c17ab7f
shoukhin93 <shoukhin1993@gmail.com>
2018-07-10 19:13:59 +0600

    allowed numbers basic design complete

--- app/src/main/java/test/example/com/devicescheduling/AllowedNumbers.java
+++ app/src/main/java/test/example/com/devicescheduling/AllowedNumbers.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -40,6 +41,10 @@ public class AllowedNumbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allowed_numbers);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
+
         initializeVariables();
         showAllAllowedNumbers();
 
